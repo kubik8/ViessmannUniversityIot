0e635979d78a5a3964565ac399bd3105de008b28
Kamil Babicki <babickik@gmail.com>
2016-12-08 14:41:41 +0100

    EntityFramework, migrations, UserDataService

--- Lab3/Source/UniversityIot.UsersDataAccess/Models/User.cs
+++ Lab3/Source/UniversityIot.UsersDataAccess/Models/User.cs
@@ -1,14 +1,20 @@
-﻿namespace UniversityIot.UsersDataAccess.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversityIot.UsersDataAccess.Models
 {
     public class User
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
-        
+        [MaxLength(10)]
         public string CustomerNumber { get; set; }
 
         public string Password { get; set; }
+
+        public ICollection<UserGateway> UserGateways { get; set; }
     }
 }
